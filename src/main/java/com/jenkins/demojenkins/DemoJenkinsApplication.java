fe3f83af47a02f230413de81a71cdbaa0a9bb366
ashishLentra <ashishya@lentra.ai>
2023-06-07 11:12:15 +0530

    first commit

---
+++ src/main/java/com/jenkins/demojenkins/DemoJenkinsApplication.java
@@ -0,0 +1,20 @@
+package com.jenkins.demojenkins;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import ch.qos.logback.classic.Logger;
+
+@SpringBootApplication
+public class DemoJenkinsApplication {
+
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.println("zzzzzzzzz");
+		SpringApplication.run(DemoJenkinsApplication.class, args);
+	}
+
+}
